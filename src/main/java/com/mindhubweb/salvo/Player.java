79020281ea34ec4f34e6e84fccc5e32673b129e3
GuillermoC <guillermo.cornetti@gmail.com>
2020-12-02 15:16:40 -0300

    Comienzo de el juego Batala Naval

---
+++ src/main/java/com/mindhubweb/salvo/Player.java
@@ -0,0 +1,58 @@
+package com.mindhubweb.salvo;
+
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+
+    private String userName;
+
+    public Player() { }
+
+    public Player(String userName) {
+        this.userName = userName;
+    }
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return id == player.id &&
+                Objects.equals(userName, player.userName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userName);
+    }
+
+}
---
+++ src/main/java/com/mindhubweb/salvo/SalvoApplication.java
@@ -0,0 +1,25 @@
+package com.mindhubweb.salvo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SalvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalvoApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner initData(PlayerRepository playerRepository) {
+		return (args) -> {
+			playerRepository.save(new Player( "j.bauer@ctu.gov" ));
+			playerRepository.save(new Player("c.obrian@ctu.gov"));
+			playerRepository.save(new Player("kim_bauer@gmail.com "));
+			playerRepository.save(new Player("t.almeida@ctu.gov"));
+		};
+	}
+}
